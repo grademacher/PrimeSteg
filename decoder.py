08ea689110e61cca14c225d7e33ad0e7229c6ecc
grademacher <rademacher.garrett@gmail.com>
2018-10-10 18:14:43 -0400

    messages can now be encrypted and the primes pulled out of the encrypted image. Auto decrypt is a piece of hot garbage.

---
+++ decoder.py
@@ -0,0 +1,95 @@
+from PIL import Image
+import numpy as np
+from pycipher import SimpleSubstitution as SimpleSub
+import random
+import re
+from ngram_score import ngram_score
+fitness = ngram_score('quadgrams.txt')
+
+
+def is_prime(n):
+    # Corner cases
+    if (n <= 1):
+        return False
+    if (n <= 3):
+        return True
+
+    # This is checked so that we can skip
+    # middle five numbers in below loop
+    if (n % 2 == 0 or n % 3 == 0):
+        return False
+
+    i = 5
+    while (i * i <= n):
+        if (n % i == 0 or n % (i + 2) == 0):
+            return False
+        i = i + 6
+
+    return True
+
+
+path = r"C:\Users\radem\PycharmProjects\PrimeSteg\new.png"
+img = Image.open(path)
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+
+ary = np.array(img)
+rows = ary.shape[0]
+cols = ary.shape[1]
+depth = ary.shape[2]
+
+hidden_message = []
+
+for i in range(0, rows):
+    for j in range(0, cols):
+        for k in range(0, depth):
+            if ary[i, j, k] < 102:
+                if is_prime(ary[i, j, k]):
+                        hidden_message.append(ary[i, j, k])
+
+print(hidden_message)
+encrypted_message = ""
+
+for prime in hidden_message:
+    encrypted_message += (alphabet[primes.index(prime)])
+
+print(encrypted_message)
+
+ctext=encrypted_message
+ctext = re.sub('[^A-Z]','',ctext.upper())
+
+maxkey = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+maxscore = -99e9
+parentscore,parentkey = maxscore,maxkey[:]
+
+# keep going until we are killed by the user
+i = 0
+while 1:
+    i = i+1
+    random.shuffle(parentkey)
+    deciphered = SimpleSub(parentkey).decipher(ctext)
+    parentscore = fitness.score(deciphered)
+    count = 0
+    while count < 1000:
+        a = random.randint(0,25)
+        b = random.randint(0,25)
+        child = parentkey[:]
+        # swap two characters in the child
+        child[a],child[b] = child[b],child[a]
+        deciphered = SimpleSub(child).decipher(ctext)
+        score = fitness.score(deciphered)
+        # if the child was better, replace the parent with it
+        if score > parentscore:
+            parentscore = score
+            parentkey = child[:]
+            count = 0
+        count = count+1
+    # keep track of best score seen so far
+    if parentscore>maxscore:
+        maxscore,maxkey = parentscore,parentkey[:]
+        print('\nbest score so far:',maxscore,'on iteration',i)
+        ss = SimpleSub(maxkey)
+        print('    best key: '+''.join(maxkey))
+        print('    plaintext: '+ss.decipher(ctext))
--- encoder.py
+++ encoder.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import random
+import math
 
 
 def is_prime(n):
@@ -42,13 +43,23 @@ message = message.replace(" ", "")
 message = message.lower()
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-substitute_alphabet = randomize(alphabet, alphabet.size)
-path = r"C:\Users\radem\PycharmProjects\PrimeSteg\test1.jpg"
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+
+randomize(primes, len(primes))
+
+prime_message = []
+
+for c in message:
+    prime_message.append(primes[alphabet.index(c)])
+
+print(prime_message)
+path = r"C:\Users\radem\PycharmProjects\PrimeSteg\test2.jpg"
 img = Image.open(path)
 
 ary = np.array(img)
 
-print(ary.shape)
+#print(ary.shape)
+#print(ary)
 
 rows = ary.shape[0]
 cols = ary.shape[1]
@@ -64,3 +75,38 @@ for i in range(0, rows):
                 else:
                     if is_prime(ary[i, j, k]):
                         ary[i, j, k] = ary[i, j, k] - 1
+
+
+total_cells = rows*cols
+insertion_interval = total_cells/len(prime_message)
+insertion_interval = math.floor(insertion_interval)
+#print(total_cells)
+#print(insertion_interval)
+
+ary.resize(total_cells, 3)
+#print(ary)
+
+for i in range(0, len(prime_message)):
+    value = prime_message[i]
+    closest_value = ary[i, 0]
+    closest_index = [i, 0]
+    for j in range((i*insertion_interval), ((i*insertion_interval)+insertion_interval)):
+        for k in range(0, 3):
+            if np.abs(value - closest_value) > np.abs(value - ary[j, k]):
+                closest_value = ary[j, k]
+                closest_index = [j, k]
+    ary[closest_index[0], closest_index[1]] = value
+
+#print(ary)
+
+ary.resize(rows, cols, 3)
+for i in range(0, rows):
+    for j in range(0, cols):
+        for k in range(0, depth):
+            if ary[i, j, k] < 102:
+                if is_prime(ary[i, j, k]):
+                        print(ary[i, j, k])
+
+hidden_image = Image.fromarray(ary, 'RGB')
+
+hidden_image.save("new.png")
